b30e122c8710dc8c880343b28a580c5012e89bbe
XiaochenCui <jcnlcxc.new@gmail.com>
2018-07-04 19:44:29 +0800

    Add some exercises

---
+++ chapter-1/exercise1-6.scm
@@ -0,0 +1,31 @@
+#|
+The program will fall into an infinite loop
+
+explanation:
+`new-if` is a function. When a function is called, it
+evaluates all the arguments
+|#
+(define (new-sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
