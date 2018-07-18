2e8a486e9d8da6009b866405704aa0a52bf98d6d
XiaochenCui <jcnlcxc.new@gmail.com>
2018-07-18 19:48:36 +0800

    update

--- chapter-1/count-change-tree-recusive.scm
+++ chapter-1/count-change-tree-recusive.scm
@@ -1,14 +1,18 @@
+#lang scheme
 (define (count-change amount)
-  (cc amount 5))
+  (cc amount 5 #f))
 
-(define (cc amount kinds-of-coins)
+(define (cc amount kinds-of-coins is-tail)
+  (print (list "cc" amount kinds-of-coins) is-tail)
   (cond ((= amount 0) 1)
         ((or (< amount 0) (= kinds-of-coins 0)) 0)
         (else (+ (cc amount
-                     (- kinds-of-coins 1))
+                     (- kinds-of-coins 1)
+                     #t)
                  (cc (- amount
                         (first-denomination kinds-of-coins))
-                     kinds-of-coins)))))
+                     kinds-of-coins
+                     #f)))))
 
 (define (first-denomination kinds-of-coins)
   (cond ((= kinds-of-coins 1) 1)
@@ -16,3 +20,17 @@
         ((= kinds-of-coins 3) 10)
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
+
+(define (print l (new-line #t))
+  (display (apply string-append (format-string l)))
+  (unless new-line (display "\n")))
+
+(define (format-string l)
+  (map (lambda (s) (string-append (number-to-string s) " ")) l))
+
+(define (number-to-string n)
+  (if (number? n)
+      (number->string n)
+      n))
+
+(count-change 11)
